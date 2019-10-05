122fda52b740286f114746f1ad3cd6edac7a929e
Vitali Satsura <vitali.satsura@gmail.com>
2019-10-05 13:21:35 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let openBrackets = [];
+
+  if (str.length % 2 !== 0)
+    return false;
+
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(element => {
+      if (openBrackets[openBrackets.length - 1] == element[0] && str[i] == element[1]) {
+        openBrackets.pop();
+      } else if (str[i] == element[0]) {
+        openBrackets.push(str[i]);
+      }
+    });
+  }
+
+  if (openBrackets.length === 0)
+    return true;
+  else
+    return false;
+
 }
